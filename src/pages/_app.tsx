d144e4ba0c0cae7371356b75179e0f4ff7e43218
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2022-11-20 12:40:10 -0300

    FRONTEND - Ajuste de estilos e arquivos root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import type { AppProps } from 'next/app'
-import { theme } from '../styles/theme'
-import "@fontsource/poppins"
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import "@fontsource/poppins";
+import "@fontsource/righteous";
+import type { AppProps } from 'next/app';
+import { theme } from '../styles/theme';
+import "../styles/default.scss";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { makeServer } from '../services/mirage';
+
+const queryClient = new QueryClient();
+
+makeServer();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,43 +1,33 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import { mode } from '@chakra-ui/theme-tools';
+
+const config: ThemeConfig  = {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+};
 
 export const theme = extendTheme({
+    config,
     colors: {
         gray: {
-            "900": "#0d1117",
-            "800": "#181B23",
-            "700": "#1F2029",
-            "600": "#4B4D63",
-            "500": "#616480",
-            "400": "#797D9A",
-            "300": "#9699B0",
-            "200": "#B3B5C6",
-            "100": "#D1D2DC",
-            "50": "#EEEEF2",
+            900: "#0d1117",
+            800: "#181B23",
+            700: "#1F2029",
+            600: "#4B4D63",
+            500: "#616480",
+            400: "#797D9A",
+            300: "#9699B0",
+            200: "#B3B5C6",
+            100: "#D1D2DC",
+            50: "#F2F3F5",
         },
         primary: {
-            "900": 'rgba(238,55,77,1)',
-            "800": 'rgba(246,92,110,1)'
+            900: 'rgba(238,55,77,1)',
+            100: 'rgba(246,92,110,1)'
         },
-        primary_gradient: {
-            "900": 'linear-gradient(58deg, rgba(238,55,77,1) 0%, rgba(246,92,110,1) 100%)'
-        },
-        linkedin_gradient: {
-            "900": 'linear-gradient(90deg, rgba(111, 139, 231, 1) 0%, rgba(47, 41, 157, 1) 100%)'
-        },
-        github_gradient: {
-            "900": 'linear-gradient(58deg, #1f9b95 8%, #3983b4 98%);'
-        }
     },
     fonts: {
-        heading: `'Poppins'`,
+        heading: `'Righteous'`,
         body: `'Poppins'`,
     },
-    styles: {
-        global: {
-            body: {
-                bg: 'gray.900',
-                color: 'gray.50',
-            }
-        }
-    }
-})
+});
