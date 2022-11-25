d2c33d34b21bdb8721f09c4b246ff00c37d766ba
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2022-11-24 21:51:14 -0300

    [DEPLOY] FT - Substituindo  @prismicio/next@0.1

--- src/services/prismicio.ts
+++ src/services/prismicio.ts
@@ -1,18 +1,17 @@
-import * as prismic from '@prismicio/client'
-import * as prismicNext from '@prismicio/next'
-import { DefaultOptions } from 'react-query'
-import sm from '../../sm.json'
+import * as prismic from '@prismicio/client';
+import * as prismicNext from '@prismicio/next';
+import sm from '../../sm.json';
 
 export interface PrismicConfig {
-    req?: prismic.HttpRequestLike;
-    previewData?: Date | any;
+  req?: prismic.HttpRequestLike;
+  previewData?: Date | any;
 }
 
 export const repositoryName = prismic.getRepositoryName(sm.apiEndpoint);
 
 export function createClient(config: PrismicConfig): prismic.Client {
   const client = prismic.createClient(sm.apiEndpoint, {
-    accessToken: process.env.PRISMIC_ACCESS_TOKEN ,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
     ...config,
   })
 
