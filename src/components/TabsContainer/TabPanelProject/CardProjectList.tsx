48da9689284ae11e9403472e36dc9181a637d4a0
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2022-12-28 18:49:56 -0300

    [FRONTEND] FT - implementacao de filtro nos projetos

--- src/components/TabsContainer/TabPanelProject/CardProjectList.tsx
+++ src/components/TabsContainer/TabPanelProject/CardProjectList.tsx
@@ -1,20 +1,21 @@
-import { Box, Button, SimpleGrid, Skeleton, Stack } from "@chakra-ui/react";
+import { Box, Button, CloseButton, Input, SimpleGrid, Skeleton, Stack, Text, useColorModeValue } from "@chakra-ui/react";
 import { useEffect, useState } from 'react';
 import { Project, ProjectsProps } from "../../../types";
 import CardProject from "./CardProject";
 
 export default function CardProjectList({ projectsPagination }: ProjectsProps) {
     const [projects, setProjects] = useState<Project[]>(projectsPagination.results);
-
+    const [search, setSearch] = useState('');
+    const [nextPage, setNextPage] = useState(projectsPagination.next_page);
+    const [currentPage, setCurrentPage] = useState(1);
     const [isLoad, setIsLoad] = useState(false);
+    const colorModeValue = useColorModeValue('gray.50', 'gray.700');
+
     useEffect(() => {
         setTimeout(() => {
             setIsLoad(true);
         }, 3600);
     }, []);
-
-    const [nextPage, setNextPage] = useState(projectsPagination.next_page);
-    const [currentPage, setCurrentPage] = useState(1);
 
     async function handleNextPage() {
         if (currentPage !== 1 && nextPage === null) {
@@ -29,7 +30,7 @@ export default function CardProjectList({ projectsPagination }: ProjectsProps) {
 
         const newProjects = projectResults.results.map(project => {
 
-            const buyProject = String(project.data.buy_project.url); 
+            const buyProject = String(project.data.buy_project.url);
 
             return {
                 uid: project.uid,
@@ -38,7 +39,7 @@ export default function CardProjectList({ projectsPagination }: ProjectsProps) {
                         url: project.data.banner.url,
                     },
                     title: project.data.title,
-                    skills: project.data.skills.map((skill)=> {
+                    skills: project.data.skills.map((skill) => {
                         return {
                             name_skills: skill.name_skills,
                         };
@@ -60,18 +61,66 @@ export default function CardProjectList({ projectsPagination }: ProjectsProps) {
         setProjects([...projects, ...newProjects]);
     };
 
+    const filteredProjects = search.length > 0
+        ? projects.filter(project => project.uid.includes(search))
+        : [];
+
+    function clearSearchInput() {
+        search.length > 0
+            ? setSearch('')
+            : search;
+    }
+
+    console.log(filteredProjects)
+
     return (
         <Stack spacing='2rem'>
+            <Box position='relative' maxW='40%'>
+                <Input
+                    variant='filled'
+                    placeholder='Search by a project name'
+                    
+                    bgColor={colorModeValue}
+                    _focus={{ borderColor: colorModeValue }}
+                    onChange={e => setSearch(e.target.value)}
+                    value={search}
+                />
+
+                {search.length > 0 && (
+                    <CloseButton
+                        alignSelf='flex-start'
+                        position='absolute'
+                        right='6px'
+                        top={1}
+                        onClick={clearSearchInput}
+                    />
+                )}
+            </Box>
+
             <SimpleGrid
                 minChildWidth={240}
                 spacing={20}
             >
-                {projects.map(project => (
-                    <Skeleton key={project.uid} isLoaded={isLoad}>
-                        <CardProject key={project.uid} results={project} />
-                    </Skeleton>
-                ))}
+                {search.length > 0
+                    ? (
+                        filteredProjects.map((project) => {
+                            return (
+                                <Skeleton key={project.uid} isLoaded={isLoad}>
+                                    <CardProject key={project.uid} results={project} />
+                                </Skeleton>
+                            )
+                        })
+                    ) : (
+                        projects.map(project => (
+                            <Skeleton key={project.uid} isLoaded={isLoad}>
+                                <CardProject key={project.uid} results={project} />
+                            </Skeleton>
+                        ))
+                    )
+                }
             </SimpleGrid>
+
+            {filteredProjects.length == 0 && search.length > 0 && (<Text textAlign='center'>Oppss...</Text>)}
 
             {nextPage && (
                 <Box textAlign='center'>
